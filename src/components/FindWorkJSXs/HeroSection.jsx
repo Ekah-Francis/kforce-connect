f6d982961db979688cecfd54f7adbe01c431d061
Ekah-Francis <forrecivingcontact@gmail.com>
2025-01-04 02:13:43 +0100

    first commit

---
+++ src/components/FindWorkJSXs/HeroSection.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import "../../CSS/FindWorkCSS/HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div id="site-content" className="page-job-seeker">
+      <section className="page-hero-image" role="region">
+        <article className="hero-content" role="article">
+          <strong>Helping You Find Tomorrow's Opportunities, Today!</strong>
+          <p>&nbsp;</p>
+          <p style={{ textAlign: "center", marginBottom: "6rem" }}>
+            <span style={{ fontSize: "14pt" }}>
+              <a
+                rel="noopener"
+                href="/search-jobs"
+                title="Search Jobs"
+                className="btn-filled-white-lochmara"
+              >
+                Search Jobs
+              </a>
+              &nbsp; &nbsp;
+              <a
+                rel="noopener"
+                href="/submit-resume"
+                title="Submit Your Resume"
+                className="btn-filled-white-lochmara"
+              >
+                Submit Resume
+              </a>
+            </span>
+          </p>
+        </article>
+      </section>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/LandingPageJSXs/HeroSection.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "../../CSS/LandingPageCSS/HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      {/* Video Background */}
+      <div className="video-background">
+        <video autoPlay loop muted playsInline className="video-element">
+          <source src="/kforce-vid.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+
+      {/* Gradient Overlay */}
+      <div className="hero-video-gradient"></div>
+
+      {/* Dark Mask */}
+      <div className="hero-image-mask"></div>
+
+      {/* Hero Content */}
+      <div className="hero-content">
+        <article className="content-container">
+          <span>
+            Let&apos;s <br />
+            transform <br /> business,
+            <br /> together
+          </span>
+          <br />
+          <br />
+          <p>
+            <a
+              href="/about"
+              title="Together Toward Tomorrow"
+              className="btn-filled"
+            >
+              Explore Trends
+            </a>
+          </p>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
